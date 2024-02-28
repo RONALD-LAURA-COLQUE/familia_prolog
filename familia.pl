fed642e38aef029eeb7ec24c426790a4da499dda
unknown <lauracolqueronaldnicolas@gmail.com>
2024-02-27 21:23:27 -0400

    CLASE DEL 27 DE FEBRERP PROLOG

---
+++ familia.pl
@@ -0,0 +1,52 @@
+cristiano.
+mariel.
+juan.
+bautista.
+cristal.
+mario.
+alejandra.
+adrian.
+luciano.
+alvaro.
+
+esHombre(cristiano).
+esHombre(juan).
+esHombre(bautista).
+esHombre(mario).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(alvaro).
+
+esMujer(mariel).
+esMujer(cristal).
+esMujer(alejandra).
+
+esHijo(juan).
+esHijo(bautista).
+esHijo(adrian).
+esHijo(luciano).
+esHijo(alvaro).
+
+esHija(cristal).
+esHija(alejandra).
+
+esHijoOHija(cristiano, juan).
+esHijoOHija(cristiano, bautista).
+
+esHijoOHija(mariel, cristal).
+esHijoOHija(mariel, bautista).
+
+esHijoOHija(bautista, alejandra).
+esHijoOHija(bautista, adrian).
+
+esHijoOHija(cristal, adrian).
+esHijoOHija(cristal, alejandra).
+esHijoOHija(cristal, luciano).
+
+esHijoOHija(bautista, luciano).
+
+esHijoOHija(mario, alvaro).
+
+esHijoOHija(alejandra, alvaro).
+
+
